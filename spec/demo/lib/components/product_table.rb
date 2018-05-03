06c25a0dfd2fa811b5a157d3877d3ac5eabfce9b
Bartek Wilczek <bartek.wilczek@toptal.com>
2018-05-03 11:59:40 +0200

    Refactor table_spec

--- spec/demo/lib/components/product_table.rb
+++ spec/demo/lib/components/product_table.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 require 'watir_pump'
 
 class ProductTable < WatirPump::Component
+  extend Forwardable
+
+  delegate Enumerable.instance_methods(false) => :data
+  delegate %i[[] empty? each] => :data
+
   query :cols, -> { node.thead.ths.map(&:text) }
 
   def data
-    ret = []
     column_names = cols
-    node.tbody.trs.each do |row|
-      ret_row = {}
-      (0...column_names.count).each do |i|
-        ret_row[column_names[i]] = row.tds[i].text
+    [].tap do |ret|
+      root.tbody.trs.each do |row|
+        ret_row = {}
+        (0...column_names.count).each do |i|
+          ret_row[column_names[i]] = row.tds[i].text
+        end
+        ret << ret_row
       end
-      ret << ret_row
     end
-    ret
   end
 end
--- spec/demo/table_spec.rb
+++ spec/demo/table_spec.rb
@@ -3,16 +3,24 @@
 require_relative 'lib/pages/index_page'
 
 RSpec.describe 'Table' do
-  it '#cols' do
+  specify '#cols' do
     IndexPage.open do
-      expect(products.cols).to include('name', 'price', 'qty')
+      expect(products.cols).to contain_exactly('name', 'price', 'qty')
     end
   end
 
-  it '#data' do
+  specify '#data' do
     IndexPage.open do
       expect(products.data.count).to eq 4
       expect(products.data.first['name']).to eq 'Hammer'
+    end
+  end
+
+  it 'delegates to #data' do
+    IndexPage.open do
+      expect(products.count).to eq 4
+      expect(products.first['name']).to eq 'Hammer'
+      expect(products[0]['name']).to eq 'Hammer'
     end
   end
 end
